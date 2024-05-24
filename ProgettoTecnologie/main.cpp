4ad54dfff2530a0e1db2e5f322f941f04163bbc6
brandovardi <brandovardipietro@outlook.it>
2024-05-24 23:29:19 +0200

    il programma riesce ad intercettare correttamente la pressione dei tasti e riesco a prendere anche il tasto premuto

--- ProgettoTecnologie/main.cpp
+++ ProgettoTecnologie/main.cpp
@@ -1,8 +1,56 @@
+#include <Windows.h>
 #include <iostream>
+#include <string>
+#include <fstream>
 
+#pragma comment(lib, "user32.lib") // user32.dll windows' library
+
+HHOOK g_hook;
+HHOOK mouseHook;
+
+// Keyboard hook
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode == HC_ACTION && (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN))
+	{
+		KBDLLHOOKSTRUCT* pKeyboardStruct = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
+
+		bool isShiftPressed = ((GetKeyState(VK_LSHIFT) & 0x8000) != 0) || ((GetKeyState(VK_RSHIFT) & 0x8000) != 0);
+		bool isCapsLockActive = (GetKeyState(VK_CAPITAL) & 0x0001) != 0;
+
+		bool isCtrlPressed = (GetKeyState(VK_CONTROL) & 0x8000) != 0;
+		bool isAltPressed = (GetKeyState(VK_MENU) & 0x8000) != 0;
+
+		int vkCode = pKeyboardStruct->vkCode;
+
+		MessageBox(NULL, (LPCWSTR)(char)vkCode, L"Titolo", 1);
+
+		std::cout << vkCode << std::endl;
+
+	}
+	// return control to the Operating System
+	return CallNextHookEx(g_hook, nCode, wParam, lParam);
+}
+
+// main function
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	HINSTANCE hInstance = GetModuleHandle(nullptr);
+	g_hook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, hInstance, 0);
 
-	return 0;
+	if (g_hook == nullptr) return EXIT_FAILURE;
+
+	MSG msg;
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	// Remove the mouse hook when the application stops running
+	UnhookWindowsHookEx(mouseHook);
+
+	UnhookWindowsHookEx(g_hook);
+
+	return ERROR_SUCCESS;
 }
